a73968e82db326e5adf5715080e22bb524c845fc
Jason Pike <jasonpike@gmail.com>
2022-11-09 22:21:41 -0600

    cleanup

--- Chip8.Disassembler/MnemonicFormatter.cs
+++ Chip8.Disassembler/MnemonicFormatter.cs
@@ -4,12 +4,12 @@ internal class MnemonicFormatter : OpcodeToTextParser
 {
     private const int MaxOpLength = 5;
     
-    public string Format(string op, params string[] args)
-    {
-        return $"{op,MaxOpLength} \t{string.Join(", ", args)}";
-    }
-
     protected MnemonicFormatter(ushort opcode) : base(opcode)
     {
+    }
+
+    protected string Format(string op, params string[] args)
+    {
+        return $"{op,MaxOpLength} \t{string.Join(", ", args)}";
     }
 }
