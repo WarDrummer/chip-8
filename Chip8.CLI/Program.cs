e33bef44fcdfc2a1b0bd5e1aa8bd9aa52e865459
Jason Pike <jasonpike@gmail.com>
2022-12-03 09:57:20 -0600

    improved CLI; added README

--- Chip8.CLI/Program.cs
+++ Chip8.CLI/Program.cs
@@ -4,19 +4,40 @@ using Chip8;
 using Chip8.IO.ConsoleKeyboard;
 using Chip8.IO.Display;
 
-var romPath = args.Length > 0 ? $"Roms/{args[0]}" : "Roms/INVADERS";
-
-// var dasm = Disassembler.Create();
-// Console.Write(dasm.Disassemble(romPath));
-
-try
+HashSet<string> GetRomNames()
 {
-    var vm = new VirtualMachine(new ConsoleDirectDisplay(), new ConsoleKeyboard());
-    vm.RunProgram(romPath);
+    return new HashSet<string>(
+        Directory.GetFiles("Roms")
+            .Select(name => name.Split("/").Last())
+        );
 }
-finally
+
+var romNames = GetRomNames();
+var selectedRomName = args.Length > 0 ? $"{args[0]}" : "INVADERS";
+if (!romNames.Contains(selectedRomName))
 {
-    Console.CursorVisible = true;
-    Console.Clear();
-    Console.WriteLine("Done");
+    Console.WriteLine($"Invalid rom: {selectedRomName}.\n\nChoose one of the following:\n{string.Join(", ", romNames)}");
+    Environment.Exit(1);
+}
+
+var romPath =  $"Roms/{selectedRomName}";
+
+if (args.Length > 1 && args[1] == "--dasm")
+{
+    var dasm = Disassembler.Create();
+    Console.Write(dasm.Disassemble(romPath));
+}
+else
+{
+    try
+    {
+        var vm = new VirtualMachine(new ConsoleDirectDisplay(), new ConsoleKeyboard());
+        vm.RunProgram(romPath);
+    }
+    finally
+    {
+        Console.CursorVisible = true;
+        Console.Clear();
+        Console.WriteLine("Done");
+    }
 }
